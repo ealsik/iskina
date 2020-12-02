77463d0a33873a061ba59b17995da01f07e0f515
iskina <iskina@aliyun.com>
2020-12-03 01:03:23 +0800

    This is iskina!!!

---
+++ src/main/java/icu/iskina/iskina/IskinaApplication.java
@@ -0,0 +1,16 @@
+package icu.iskina.iskina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author iskina
+ */
+@SpringBootApplication
+public class IskinaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IskinaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/icu/iskina/iskina/IskinaApplicationTests.java
@@ -0,0 +1,13 @@
+package icu.iskina.iskina;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IskinaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
